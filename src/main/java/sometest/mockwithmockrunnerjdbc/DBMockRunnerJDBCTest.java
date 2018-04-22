93d479c12469eabd46247bb84d22e41954d4e7d9
António Homem Ferreira <antonio.h.ferreira@gmail.com>
2018-04-22 22:29:27 +0100

    mockrunner-jdbc test prints executed queries

--- src/main/java/sometest/mockwithmockrunnerjdbc/DBMockRunnerJDBCTest.java
+++ src/main/java/sometest/mockwithmockrunnerjdbc/DBMockRunnerJDBCTest.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 import org.junit.Test;
 
 import com.mockrunner.jdbc.BasicJDBCTestCaseAdapter;
+import com.mockrunner.jdbc.ParameterSets;
 import com.mockrunner.jdbc.PreparedStatementResultSetHandler;
 import com.mockrunner.mock.jdbc.MockConnection;
 import com.mockrunner.mock.jdbc.MockResultSet;
@@ -45,6 +46,22 @@ public class DBMockRunnerJDBCTest extends BasicJDBCTestCaseAdapter {
         tq.connect();
         tq.executeQueries();
         tq.disconnect();
+        
+        System.out.println("------------------------");
+        System.out.println("All executed queries: ");
+        for( String sql : getExecutedSQLStatements() )
+        {
+            System.out.println("-->"+sql+"<--");
+            StringBuilder sb = new StringBuilder("\twith params: ");
+            ParameterSets params = getExecutedSQLStatementParameterSets(sql);
+            for(int i=0;i<params.getNumberParameterSets();i++)
+            {
+                sb.append(params.getParameterSet(i));
+                sb.append(",");
+            }
+            System.out.println(sb.toString());
+        }
+        System.out.println("------------------------");
         
         verifySQLStatementExecuted(TestQueries.QUERY_PREPARED_STATEMENT);
         verifySQLStatementExecuted(TestQueries.QUERY_INSERT);
